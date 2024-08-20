64ad54fc5307599343c2787cf826886464ad43b1
st4pzz <ramellajr@gmail.com>
2024-08-20 15:35:31 -0300

    api

---
+++ api.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/query', methods=['GET'])
+def query():
+    data = pd.read_csv()
+    query = request.args.get('query')
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(data["Description"])
+    Q = vectorizer.transform(query)
+    R = X @ Q.T
+    R = R.toarray().flatten()
+    idx = R.argsort()[-10:]
+    lista = []
+    for i in idx:
+        dici = {}
+        dici['title'] = data.iloc[i]["Crypto_name"]
+        dici['content'] = data.iloc[i]["Description"]
+        dici['relevance'] = idx[i]
+        lista.append(dici)
+
+    return jsonify({
+        'results': lista,
+        'message' : 'OK'
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
